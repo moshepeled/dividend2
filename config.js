0988c767ca1702cc24e78280b7902b4b7d42a214
Moshe Peled <moshe.peled@gmail.com>
2020-01-18 13:21:14 +0200

    update config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://moshe:brasil111@ds013405.mlab.com:13405/csdata',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://',
     PORT: process.env.PORT || 3001
 };
